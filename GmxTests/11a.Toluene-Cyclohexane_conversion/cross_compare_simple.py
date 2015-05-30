14f9d7389f82d8d6fdc8c01a6965c03168e7d92c
davidlmobley <dmobley@gmail.com>
2015-05-29 17:37:29 -0700

    Adding a third test which looks at PME energetics.

--- GmxTests/11a.Toluene-Cyclohexane_conversion/cross_compare_simple.py
+++ GmxTests/11a.Toluene-Cyclohexane_conversion/cross_compare_simple.py
@@ -2,11 +2,11 @@
 
 import chemistry as chem
 from simtk.openmm import *; from simtk.openmm.app import *; from simtk.unit import *
+import simtk.unit as u
 
 #Jason's original version - AMBER vs GROMACS with same (original) coordinates)
 l = chem.amber.AmberParm('toluene_cyclohexane_10_500.prmtop', 'toluene_cyclohexane_10_500.inpcrd')
 k = chem.load_file('toluene_cyclohexane_10_500_parmed.top')
-[a.name for a in l.atoms] == [a.name for a in k.atoms]
 sysl = l.createSystem()
 sysk = k.createSystem()
 conl = Context(sysl, VerletIntegrator(0.001))
@@ -25,7 +25,6 @@ gro = chem.gromacs.GromacsGroFile.parse( 'toluene_cyclohexane_10_500_parmed.gro'
 gromacsparm.box = gro.box
 gromacsparm.positions = gro.positions
 #Check atom names
-[ a.name for a in amberparm.atoms ] == [ a.name for a in gromacsparm.atoms ]
 #Create systems
 sysa = amberparm.createSystem()
 sysg = gromacsparm.createSystem()
@@ -36,5 +35,16 @@ cong.setPositions(gromacsparm.positions)
 print(chem.openmm.utils.energy_decomposition(amberparm, cona))
 print(chem.openmm.utils.energy_decomposition(gromacsparm, cong))
 
-
-#Could generalize by making a whole matrix of systems...
+#Same as first case but with PME/PBC; now adding copying of box info to k from l
+l = chem.amber.AmberParm('toluene_cyclohexane_10_500.prmtop', 'toluene_cyclohexane_10_500.inpcrd')
+k = chem.load_file('toluene_cyclohexane_10_500_parmed.top')
+k.box=l.box
+k.positions = l.positions
+sysl = l.createSystem( nonbondedMethod=app.PME, nonbondedCutoff=1.0 * u.nanometers, constraints=None )
+sysk = k.createSystem( nonbondedMethod=app.PME, nonbondedCutoff=1.0 * u.nanometers, constraints=None )
+conl = Context(sysl, VerletIntegrator(0.001))
+conk = Context(sysk, VerletIntegrator(0.001))
+conl.setPositions(l.positions)
+conk.setPositions(l.positions)
+print(chem.openmm.utils.energy_decomposition(l, conl))
+print(chem.openmm.utils.energy_decomposition(k, conk))
